45d5feb5dc93d6cb87463ddc7bf17620ae224378
Alejdmc <damonroy86@ucatolica.edu.co>
2025-06-12 03:41:28 -0500

    inicio rutas, esperando models

---
+++ routes/vuelos.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter, Form, File, UploadFile, Depends, HTTPException, Response
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.future import select
+from typing import List
+from models import Usuario, Vuelo, Mascota
+from utils.connection_db import get_session
+
+router = APIRouter(prefix="/vuelos", tags=["vuelos"])
