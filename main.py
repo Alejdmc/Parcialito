8354b6843c4d38afe7c6b67226c8a4b03e6ff755
Alejdmc <damonroy86@ucatolica.edu.co>
2025-06-12 03:53:39 -0500

    avance en rutas

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(ruta_usuarios)
 
 
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
