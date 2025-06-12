a7c832be913d44f87777626c5e6a0db8ceea947e
SamuelPreciado <sepreciado02@ucatolica.edu.co>
2025-06-11 19:12:22 -0500

    se crea los modelos de usuario y mascota

---
+++ models.py
@@ -0,0 +1,14 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+class Usuarios(SQLModel, table=True):
+    usuario_id: Optional[int] = Field(default=None, primary_key=True)
+    nombre: str
+    mascotas: list["Mascotas"] = Field(default_factory=list)
+
+
+class Mascotas(SQLModel, table=True):
+    mascota_id: Optional[int] = Field(default=None, primary_key=True)
+    nombre: str
+    raza: str
+    vuelo: str
+    dueño: "Usuarios" = Field(default_factory="Usuarios")
