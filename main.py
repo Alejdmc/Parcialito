1ef92025e3f6483db07dfb37875af8b5797de393
Alejdmc <damonroy86@ucatolica.edu.co>
2025-06-12 03:37:24 -0500

    inicio de main y creo las rutas

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.future import select
+from sqlmodel.ext.asyncio.session import AsyncSession
+from routes.vuelos import router as ruta_vuelos
+from routes.usuarios import router as ruta_usuarios
+from routes.mascotas import router as ruta_mascotas
+
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+app.include_router(ruta_mascotas)
+app.include_router(artistas_vuelos)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.include_router(ruta_usuarios)
+
 
 
 @app.get("/")
